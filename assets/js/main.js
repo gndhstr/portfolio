c048993bbcc24bc65a3df5babfe95c396a3c89c2
gndhstr <ghandisatria@gmail.com>
2022-11-20 15:30:47 +0700

    update fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: UpConstruction - v1.2.1
-* Template URL: https://bootstrapmade.com/upconstruction-bootstrap-construction-website-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 document.addEventListener('DOMContentLoaded', () => {
   "use strict";
 
